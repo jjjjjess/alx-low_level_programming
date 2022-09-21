274fe31d203d920503f27543bef6b04b9dcc468d
Jessica Jere <jessicanjere@gmail.com>
2022-09-21 14:53:28 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ *
+ * @int: input letter
+ *
+ * Return: Always success
+ */
+
+int _abs(int);
+{
+	return (ab * ((ab > 0) - (ab < 0)));
+}
