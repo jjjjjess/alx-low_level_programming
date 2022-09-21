6a8835d6f1ad783894e92c68795728d7c04e765b
Jessica Jere <jessicanjere@gmail.com>
2022-09-21 15:37:42 -0700

    task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * times_table - prints 9 times table
+ *
+ * rw = row, col = column, d = digits
+ *
+ * Return: Always success
+ */
+
+void times_table(void)
+{
+	int rw, col, d;
+
+	for (rw = 0; rw <= 9; rw++)
+
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+
+		for (col = 1; col <= 9; col++)
+		{
+			d = (rw * col);
+
+			if ((d / 10) > 0)
+			{
+				_putchar((d / 10) + '0');
+			}
+
+			else
+			{
+				_putchar(' ');
+			}
+		}
+	}
+}
