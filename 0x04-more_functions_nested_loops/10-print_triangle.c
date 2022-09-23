aec3e2645fd527da87413572cfbe67d6be8270f0
Jessica Jere <jessicanjere@gmail.com>
2022-09-23 02:37:22 -0700

    .

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_triangle - Prints a triangle
+ * @size: sets the size of the triangle
+ * Return: triangle of '#'s
+ */
+
+void print_triangle(int size)
+{
+	int sp, ro, tr;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+
+	else
+	{
+
+		for (ro = 0; ro <= (size - 1); ro++)
+		{
+
+			for (sp = 0; sp < (size - 1) - ro; sp++)
+			{
+				_putchar(' ');
+			}
+
+			for (tr = 0; tr <= ro; tr++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
