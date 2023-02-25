396eb93f185577ed3df12183aa0e16453aec9c59
Jessica <jessicanjere@gmail.com>
2023-02-25 03:14:25 -0800

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main -Prints the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char letter
+		for (letter = 'a'; letter <= 'z'; letter++)
+			putchar(letter);
+			puthcar('\n');
+	return (0);
+}
