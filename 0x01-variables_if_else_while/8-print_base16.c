1215a60267cbda3fca1b28f2490508eaeb556192
Jessica <jessicanjere@gmail.com>
2023-02-27 04:28:59 -0800

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+	char letter;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar((num % 10) + '0');
+	}
+
+	for (letter = 'a'; letter <= 'f'; lettter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
