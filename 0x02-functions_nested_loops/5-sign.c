a359d470d5c46da43a66bd1342ce7a82db5143c5
Jessica <jessicanjere@gmail.com>
2023-03-02 06:22:24 -0800

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,18 +10,16 @@
 
 int print_sign(int n)
 {
-	int n;
-
 	if (n > 0)
 	{
 		_putchar('+');
+		return (1);
 	}
-	return (1);
 	else if (n < 0)
 	{
 		_putchar('-');
+		return (-1);
 	}
-	return (-1);
 	else
 	{
 		_putchar('0');
