1b99ae41544ebd69d10c5708ee2b36233bd249bc
Jessica <jessicanjere@gmail.com>
2023-03-02 03:32:32 -0800

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,7 @@
 #include "main.h"
 
 /**
- * main - Prints the alphabet in lowercase
- *
- * Return: Always 0 (Success)
+ * print_alphabet - Prints the alphabet in lowercase
  */
 
 void print_alphabet(void);
@@ -15,5 +13,5 @@ void print_alphabet(void);
 		_putchar(letter);
 	}
 	_putchar('\n');
-	return (0);
+
 }
