0bb7db6ceeeffca59eb955ac4fbd3ff5b768415b
Jessica <jessicanjere@gmail.com>
2023-03-02 06:42:40 -0800

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - returns absolute value of integer
+ *
+ * @a: parameter to be checked
+ *
+ * Return: Always a
+ */
+
+int _abs(int a)
+{
+	if (a < 0)
+		a = -(a);
+	else if (a >= 0)
+		a = a
+	else
+		return (a);
+}
